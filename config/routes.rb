0cfd4f452c04b0dfd9374403f5cca03ce6d24fcd
djShrek <boho527@gmail.com>
2016-08-17 00:08:27 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'graph/:category', :to => 'graph#show'
+    end
+  end
+
+  namespace :api do
+    namespace :v1 do
+      get '/category/:category', :to => 'category#show'
+    end
+  end
+end
